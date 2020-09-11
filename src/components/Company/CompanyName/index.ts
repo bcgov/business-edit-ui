2f1a40f55fad02b33f0bc8142b1b3f9a7c5962d9
Cameron Bowler <cameron@freshworks.io>
2020-09-11 15:33:23 -0700

    Fixed Imports

--- src/components/Company/CompanyName/index.ts
+++ src/components/Company/CompanyName/index.ts
@@ -1,6 +1,1 @@
 export { default as CorrectNameOptions } from './CorrectNameOptions.vue'
-<<<<<<< HEAD
-=======
-export { default as CorrectNameRequest } from './CorrectNameRequest.vue'
-export { default as CorrectCompanyName } from './CorrectCompanyName.vue'
->>>>>>> Correct Name and UI Updates
