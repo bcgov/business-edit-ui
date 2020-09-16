55da5fd0a0e7af84a52199d551e7ea97465dfc4b
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2020-09-16 15:18:40 -0700

    Agreement type correction - first commit

--- src/components/IncorporationAgreement/AgreementType.vue
+++ src/components/IncorporationAgreement/AgreementType.vue
@@ -4,41 +4,72 @@
       <v-card flat>
         <!-- Summary Header -->
         <div class="agreement-summary-header">
-            <v-icon>mdi-handshake</v-icon>
-            <label class="agreement-summary-title">
-              <strong>Incorporation Agreement and Benefit Company Articles</strong>
-            </label>
+          <v-icon>mdi-handshake</v-icon>
+          <label class="agreement-summary-title">
+            <strong>Incorporation Agreement and Benefit Company Articles</strong>
+          </label>
         </div>
 
-        <!-- Summary Warning -->
-        <div v-if="showErrorSummary" class="agreement-invalid-message">
-            <span>
-            <v-icon color="blue darken-2">mdi-information-outline</v-icon>
-            This step is not complete.
-            <router-link id="router-link" :to="{ path: '/incorporation-agreement' }">
-                Return to this step to complete it.
-            </router-link>
-            </span>
-        </div>
         <!-- Summary Content -->
-        <div v-else class="summary-desc">
-            <div><v-icon color="green" class="agreement-valid-icon">mdi-check</v-icon></div>
-             <div>
-                {{ selectedAgreementDescription }}
+        <div v-if="!showAgreementTypeForm" class="summary-desc">
+          <div><v-icon color="green" class="agreement-valid-icon">mdi-check</v-icon></div>
+          <div>
+            <div>
+              {{ selectedAgreementDescription }}
             </div>
+            <div v-if="hasAgreementTypeChange">
+              <v-chip x-small label color="blue" text-color="white">
+                Corrected
+              </v-chip>
+            </div>
+          </div>
+          <div>
+            <v-flex md2 class="align-right">
+              <v-btn
+                v-if="!hasAgreementTypeChange"
+                text
+                color="primary"
+                id="btn-correct-agreement-type"
+                @click="showAgreementTypeForm = true">
+                <v-icon small>mdi-pencil</v-icon>
+                <span>Correct</span>
+              </v-btn>
+              <v-btn
+                v-if="hasAgreementTypeChange"
+                text
+                color="primary"
+                id="btn-undo-agreement-type"
+                @click="resetAgreementType">
+                <v-icon small>mdi-undo</v-icon>
+                <span>Undo</span>
+              </v-btn>
+            </v-flex>
+          </div>
         </div>
-      </v-card>
-    </div>
-    <div v-else>
-      <v-card flat>
-        <v-radio-group v-model="agreementType" @change="changeAgreementType" class="agreement-option-list">
-          <v-radio v-for="(item, index) in incorporationAgreementTypeResource"
-           :key="index" :value="item.code" :id="`agreement-type-${item.code}`">
-            <template slot="label">
-              <div v-html="item.description" class="agreement-option"/>
-            </template>
-          </v-radio>
-        </v-radio-group>
+        <div v-else>
+          <v-card flat>
+            <v-radio-group v-model="agreementType" class="agreement-option-list">
+              <v-radio
+                v-for="(item, index) in incorporationAgreementTypeResource"
+                :key="index"
+                :value="item.code"
+                :id="`agreement-type-${item.code}`">
+                <template slot="label">
+                  <div v-html="item.description" class="agreement-option" />
+                </template>
+              </v-radio>
+            </v-radio-group>
+            <div class="action-btns">
+              <v-btn id="done-btn" large color="primary" @click="setAgreementType">
+                <span>Done</span>
+              </v-btn>
+
+              <v-btn v-btn id="cancel-btn" large outlined color="primary" @click="resetAgreementType">
+                <span>Cancel</span>
+              </v-btn>
+            </div>
+          </v-card>
+        </div>
       </v-card>
     </div>
   </div>
@@ -46,11 +77,11 @@
 
 <script lang="ts">
 // Libraries
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Emit, Vue, Prop, Watch } from 'vue-property-decorator'
 import { Action, State } from 'vuex-class'
 
 // Interfaces
-import { ActionBindingIF } from '@/interfaces'
+import { ActionBindingIF, IncorporationFilingIF } from '@/interfaces'
 import { AgreementTypeResource } from '@/resources'
 
 @Component
@@ -61,8 +92,8 @@ export default class AgreementType extends Vue {
   @State(state => state.stateModel.incorporationAgreementStep.agreementType)
   readonly agreementTypeState: string | null
 
-  @Prop({ default: false })
-  private showErrorSummary: boolean
+  @State(state => state.stateModel.originalIA)
+  readonly originalIA!: IncorporationFilingIF
 
   @Prop({ default: false })
   private isSummary: boolean
@@ -73,6 +104,7 @@ export default class AgreementType extends Vue {
 
   // Local properties
   private agreementType: string | null = null
+  private showAgreementTypeForm = false
 
   // Lifecycle methods
   private created (): void {
@@ -89,11 +121,22 @@ export default class AgreementType extends Vue {
     Vue.nextTick(() => this.setHaveChanges(false))
   }
 
-  private changeAgreementType (): void {
+  private setAgreementType (): void {
     this.setIncorporationAgreementStepData({
       valid: !!this.agreementType,
       agreementType: this.agreementType
     })
+    this.emitHaveChanges()
+    this.showAgreementTypeForm = false
+  }
+
+  private resetAgreementType (): void {
+    this.agreementType = this.originalIA.incorporationApplication.incorporationAgreement.agreementType
+    this.setAgreementType()
+  }
+
+  private get hasAgreementTypeChange (): boolean {
+    return this.agreementType !== this.originalIA.incorporationApplication.incorporationAgreement.agreementType
   }
 
   private get selectedAgreementDescription () : string {
@@ -101,6 +144,18 @@ export default class AgreementType extends Vue {
       return this.incorporationAgreementTypeResource.find(item => item.code === this.agreementTypeState)
         .summaryDescription
     } else { return '' }
+  }
+
+  @Emit('haveChanges')
+  private emitHaveChanges (): boolean {
+    return (
+      this.hasAgreementTypeChange
+    )
+  }
+
+  @Watch('agreementTypeState')
+  private onAgreementTypeStateChange () {
+    this.agreementType = this.agreementTypeState
   }
 }
 </script>
@@ -116,12 +171,6 @@ export default class AgreementType extends Vue {
   .agreement-summary-title {
     padding-left: .5rem;
   }
-}
-
-.agreement-invalid-message {
-  padding: 1.25rem;
-  font-weight: bold;
-  color: $BCgovABlue2;
 }
 
 #agreement-summary {
@@ -146,5 +195,33 @@ export default class AgreementType extends Vue {
 .agreement-option {
   padding-top: 1rem;
   color: $gray7;
+}
+
+.action-btns {
+      display: flex;
+      justify-content: flex-end;
+      padding-bottom: 1rem;
+      padding-right:0.5rem;
+
+      .v-btn + .v-btn {
+        margin-left: 0.5rem;
+      }
+
+      .v-btn {
+        min-width: 6.5rem;
+      }
+
+      .v-btn[disabled] {
+        color: white !important;
+        background-color: #1669BB !important;
+        opacity: .2;
+      }
+}
+
+.v-size--x-small {
+  display: table;
+  margin-top: 0.5rem;
+  text-transform: uppercase;
+  font-weight: 700;
 }
 </style>
