57fe8852de86078362cf01972d5903b5638b405e
Cameron Bowler <cameron@freshworks.io>
2020-10-15 11:18:04 -0700

    Pr Updates

--- tests/unit/entityInfo.spec.ts
+++ tests/unit/entityInfo.spec.ts
@@ -15,11 +15,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(Vuetify)
 const vuetify = new Vuetify({})
+const store = getVuexStore()
+let state = store.state.stateModel
 document.body.setAttribute('data-app', 'true')
 
 describe('Entity Info component in a Correction as a named company', () => {
   let wrapper
-  let store
 
   const mockFiling = {
     header: {
@@ -47,16 +48,15 @@ describe('Entity Info component in a Correction as a named company', () => {
     }
   }
 
-  beforeEach(() => {
-    store = getVuexStore()
-    let state = store.state.stateModel
-
+  beforeAll(() => {
     state.tombstone.keycloakRoles = ['staff']
     state.businessInformation = mockFiling.business
     state.tombstone.businessId = mockFiling.business.identifier
     state.defineCompanyStep.businessContact = mockFiling.incorporationApplication.contactPoint
     state.originalIA.incorporationApplication.nameRequest = mockFiling.incorporationApplication.nameRequest
+  })
 
+  beforeEach(() => {
     const localVue = createLocalVue()
     localVue.use(VueRouter)
     const router = mockRouter.mock()
@@ -70,15 +70,15 @@ describe('Entity Info component in a Correction as a named company', () => {
     })
   })
 
-  afterEach(async () => {
-    await wrapper.destroy()
+  afterEach(() => {
+    wrapper.destroy()
   })
 
-  it('renders the EntityInfo Component', async () => {
+  it('renders the EntityInfo Component', () => {
     expect(wrapper.find(EntityInfo).exists()).toBe(true)
   })
 
-  it('displays the breadcrumb correctly as a named benefit company', async () => {
+  it('displays the breadcrumb correctly as a named benefit company', () => {
     const breadcrumbs = wrapper.findAll('.v-breadcrumbs li')
 
     const crumb1 = breadcrumbs.at(0)
@@ -86,35 +86,26 @@ describe('Entity Info component in a Correction as a named company', () => {
     const crumb2 = breadcrumbs.at(2)
     const crumb3 = breadcrumbs.at(4)
 
-    expect(crumb1.text()).toStrictEqual('Staff Dashboard')
-    expect(divider.text()).toStrictEqual('>')
-    expect(crumb2.text()).toStrictEqual('My Mock Name Inc.')
-    expect(crumb3.text()).toStrictEqual('Correction - Incorporation Application')
+    expect(crumb1.text()).toBe('Staff Dashboard')
+    expect(divider.text()).toBe('>')
+    expect(crumb2.text()).toBe('My Mock Name Inc.')
+    expect(crumb3.text()).toBe('Correction - Incorporation Application')
   })
 
-  it('renders the business name and numbers', async () => {
-    expect(wrapper.vm.$el.querySelector('#entity-legal-name').textContent)
-      .toContain('My Mock Name Inc.')
-
-    expect(wrapper.vm.$el.querySelector('#entity-business-number').textContent)
-      .toContain('1234567')
-
-    expect(wrapper.vm.$el.querySelector('#entity-incorp-number').textContent)
-      .toContain('BC1234567')
+  it('renders the business name and numbers', () => {
+    expect(wrapper.find('#entity-legal-name').text()).toContain('My Mock Name Inc.')
+    expect(wrapper.find('#entity-business-number').text()).toContain('1234567')
+    expect(wrapper.find('#entity-incorp-number').text()).toContain('BC1234567')
   })
 
-  it('renders the business contact information', async () => {
-    expect(wrapper.vm.$el.querySelector('#entity-business-email').textContent)
-      .toContain('mock@email.com')
-
-    expect(wrapper.vm.$el.querySelector('#entity-business-phone').textContent)
-      .toContain('123-456-7890')
+  it('renders the business contact information', () => {
+    expect(wrapper.find('#entity-business-email').text()).toContain('mock@email.com')
+    expect(wrapper.find('#entity-business-phone').text()).toContain('123-456-7890')
   })
 })
 
 describe('Entity Info component in a Correction as a numbered company', () => {
   let wrapper
-  let store
 
   const mockFiling = {
     header: {
@@ -128,7 +119,7 @@ describe('Entity Info component in a Correction as a numbered company', () => {
     incorporationApplication: {
       contactPoint: {
         email: 'mock@email.com',
-        phone: '123-456-7890'
+        phone: '321-456-7890'
       },
       nameRequest: {},
       offices: {},
@@ -138,16 +129,15 @@ describe('Entity Info component in a Correction as a numbered company', () => {
     }
   }
 
-  beforeEach(() => {
-    store = getVuexStore()
-    let state = store.state.stateModel
-
+  beforeAll(() => {
     state.tombstone.keycloakRoles = ['staff']
     state.businessInformation = mockFiling.business
     state.tombstone.businessId = mockFiling.business.identifier
     state.defineCompanyStep.businessContact = mockFiling.incorporationApplication.contactPoint
     state.originalIA.incorporationApplication.nameRequest = mockFiling.incorporationApplication.nameRequest
+  })
 
+  beforeEach(() => {
     const localVue = createLocalVue()
     localVue.use(VueRouter)
     const router = mockRouter.mock()
@@ -161,15 +151,15 @@ describe('Entity Info component in a Correction as a numbered company', () => {
     })
   })
 
-  afterEach(async () => {
-    await wrapper.destroy()
+  afterEach(() => {
+    wrapper.destroy()
   })
 
-  it('renders the EntityInfo Component', async () => {
+  it('renders the EntityInfo Component', () => {
     expect(wrapper.find(EntityInfo).exists()).toBe(true)
   })
 
-  it('displays the breadcrumb correctly as a named benefit company', async () => {
+  it('displays the breadcrumb correctly as a named benefit company', () => {
     const breadcrumbs = wrapper.findAll('.v-breadcrumbs li')
 
     const crumb1 = breadcrumbs.at(0)
@@ -177,28 +167,20 @@ describe('Entity Info component in a Correction as a numbered company', () => {
     const crumb2 = breadcrumbs.at(2)
     const crumb3 = breadcrumbs.at(4)
 
-    expect(crumb1.text()).toStrictEqual('Staff Dashboard')
-    expect(divider.text()).toStrictEqual('>')
-    expect(crumb2.text()).toStrictEqual('Numbered Benefit Company')
-    expect(crumb3.text()).toStrictEqual('Correction - Incorporation Application')
+    expect(crumb1.text()).toBe('Staff Dashboard')
+    expect(divider.text()).toBe('>')
+    expect(crumb2.text()).toBe('Numbered Benefit Company')
+    expect(crumb3.text()).toBe('Correction - Incorporation Application')
   })
 
-  it('renders the business name and numbers', async () => {
-    expect(wrapper.vm.$el.querySelector('#entity-legal-name').textContent)
-      .toContain('Numbered Benefit Company')
-
-    expect(wrapper.vm.$el.querySelector('#entity-business-number').textContent)
-      .toContain('7654321')
-
-    expect(wrapper.vm.$el.querySelector('#entity-incorp-number').textContent)
-      .toContain('BC7654321')
+  it('renders the business name and numbers', () => {
+    expect(wrapper.find('#entity-legal-name').text()).toContain('Numbered Benefit Company')
+    expect(wrapper.find('#entity-business-number').text()).toContain('7654321')
+    expect(wrapper.find('#entity-incorp-number').text()).toContain('BC7654321')
   })
 
-  it('renders the business contact information', async () => {
-    expect(wrapper.vm.$el.querySelector('#entity-business-email').textContent)
-      .toContain('mock@email.com')
-
-    expect(wrapper.vm.$el.querySelector('#entity-business-phone').textContent)
-      .toContain('123-456-7890')
+  it('renders the business contact information', () => {
+    expect(wrapper.find('#entity-business-email').text()).toContain('mock@email.com')
+    expect(wrapper.find('#entity-business-phone').text()).toContain('321-456-7890')
   })
 })
