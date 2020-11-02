551452a975ad8bcde70d56af4cf304c8f74c6a98
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2020-11-02 15:32:34 -0800

    Update YourCompany.vue
    - set Your Company icon to blue

--- src/components/YourCompany/YourCompany.vue
+++ src/components/YourCompany/YourCompany.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card flat id="your-company">
      <div class="define-company-header">
-        <v-icon>mdi-domain</v-icon>
+        <v-icon color="#38598A">mdi-domain</v-icon>
         <label class="define-company-title font-weight-bold">Your Company</label>
     </div>
 
