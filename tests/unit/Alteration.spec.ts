bf37d4db98cca67e90bd78bb49f2ca86108a1ad3
John Churchill <john@highwaythreesolutions.com>
2021-03-11 09:27:19 -0800

    Updated test to reflect change in value

--- tests/unit/Alteration.spec.ts
+++ tests/unit/Alteration.spec.ts
@@ -257,7 +257,7 @@ describe('Alteration component', () => {
     // Validate People And Roles
     expect(store.state.stateModel.peopleAndRolesStep.orgPeople[0].officer.firstName).toBe('CAMERON')
     expect(store.state.stateModel.peopleAndRolesStep.orgPeople[0].officer.lastName).toBe('BOWLER')
-    expect(store.state.stateModel.peopleAndRolesStep.orgPeople[0].roles[0].roleType).toBe('director')
+    expect(store.state.stateModel.peopleAndRolesStep.orgPeople[0].roles[0].roleType).toBe('Director')
 
     // Validate Share Structure
     expect(store.state.stateModel.shareStructureStep.shareClasses[0].name).toBe('Class A Shares')
