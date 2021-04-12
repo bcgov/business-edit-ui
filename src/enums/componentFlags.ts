269ee6ae176c7e5cc2f69e31144649db0bf19e4d
Cameron-freshworks <cameron@freshworks.io>
2021-04-12 09:40:40 -0700

    remove dev code

--- src/enums/componentFlags.ts
+++ src/enums/componentFlags.ts
@@ -1,7 +1,7 @@
 /**
  * Enum for component validator flags.
  * enum properties MUST match component ID to scroll correctly.
- * Order is arbitrary but follow component hierarchy.
+ * Order is arbitrary but follows component hierarchy.
  * */
 export enum ComponentFlags {
   'articles',
