56ac92567ca3d284da59d48f7c178173fa5edc45
Severin Beauvais <severin.beauvais@gmail.com>
2022-02-02 14:42:58 -0800

    - initialize token service in main instead of App - redirect for login then return here - after logout, redirect to login page - app version = 0.6.28

--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,49 +1,19 @@
 <template>
-  <sbc-signin @sync-user-profile-ready="onProfileReady()" />
+  <div />
 </template>
 
 <script lang="ts">
-// Libraries
-import { Component, Emit, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
-// Components
-import SbcSignin from 'sbc-common-components/src/components/SbcSignin.vue'
-
-/**
- * Operation:
- * 1. When this component is first loaded (ie, we are not authenticated) then the
- *    SbcSgnin component will redirect us to log in.
- * 2. When this component is reloaded (ie, we are now authenticated) then the
- *    SbcSignin component will emit "sync-user-profile-ready".
- */
-@Component({
-  components: {
-    SbcSignin
-  }
-})
+@Component({})
 export default class Signin extends Vue {
-  /** Called when user profile is ready (ie, the user is authenticated). */
-  private onProfileReady () {
-    // let App know that data can now be loaded
-    this.emitProfileReady()
-
-    if (this.$route.query.redirect) {
-      // navigate to the route we originally came from
-      this.$router.push(this.$route.query.redirect as string)
-    } else {
-      console.error('Signin page missing redirect param') // eslint-disable-line no-console
-      // redirect to Business Registry home page
-      const businessesUrl = sessionStorage.getItem('BUSINESSES_URL')
-      // assume Businesses URL is always reachable
-      window.location.assign(businessesUrl)
-    }
+  created () {
+    // redirect to BC Registry login page then return to this app
+    const loginUrl = sessionStorage.getItem('REGISTRY_HOME_URL') + 'login'
+    const baseUrl = sessionStorage.getItem('BASE_URL').replace(/\/$/, '') // remove trailing /
+    const redirect = this.$route.query.redirect
+    const returnUrl = encodeURIComponent(baseUrl + redirect)
+    window.location.assign(`${loginUrl}?return=${returnUrl}`)
   }
-
-  /** Emits Profile Ready event. */
-  @Emit('profileReady')
-  private emitProfileReady (profileReady: boolean = true): void {}
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
