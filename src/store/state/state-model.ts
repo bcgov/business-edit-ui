057da025c28613c53ee97070c3413bd9d911584f
lambert.alex <alex.m.lambert@icloud.com>
2022-07-11 07:56:21 -0700

    change type default in stateModel

--- src/store/state/state-model.ts
+++ src/store/state/state-model.ts
@@ -22,7 +22,7 @@ export const stateModel: StateModelIF = {
     haveUnsavedChanges: false,
     folioNumber: '',
     transactionalFolioNumber: '',
-    type: null
+    type: ''
   },
   completingParty: null,
   newAlteration: {
