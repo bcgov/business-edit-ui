305ded6c20c2ab46870363a7612bdf0e5653cd78
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2022-07-25 16:07:52 -0700

    Enabling the Edit UI for CO-OP#12931

--- src/interfaces/special-resolution-interfaces/special-resolution-interface.ts
+++ src/interfaces/special-resolution-interfaces/special-resolution-interface.ts
@@ -2,8 +2,7 @@ import { BusinessInformationIF, CourtOrderIF, NameRequestIF }
   from '@/interfaces/'
 import { ContactPointIF } from '@bcrs-shared-components/interfaces/'
 
-//
-//
+/** Interface for data object UI sends to API. */
 export interface SpecialResolutionIF {
   provisionsRemoved?: boolean,
   business?: BusinessInformationIF,
