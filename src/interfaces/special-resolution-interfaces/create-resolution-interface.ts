cf196454bb175f759fcb48c845b49ce752f1a112
shabeeb-aot <faiz.shabeeb@aot-technologies.com>
2022-08-10 15:56:08 -0700

    UI for Special Resolution Form Details#12926

--- src/interfaces/special-resolution-interfaces/create-resolution-interface.ts
+++ src/interfaces/special-resolution-interfaces/create-resolution-interface.ts
@@ -2,7 +2,7 @@ import { SigningPersonIF } from '@/interfaces'
 
 // specical resolution form inrterface
 export interface CreateResolutionIF {
-  resolutionConfirmed: boolean
+  resolutionConfirmed?: boolean
   resolutionDate?: string // YYYY-MM-DD
   resolution?: string
   signatory?: SigningPersonIF
