2d97594a91a6f2a8e97f03f7b3b514a3e8642276
Travis Semple <travis8814@gmail.com>
2022-08-31 09:28:34 -0700

    Fix for Court Order Poa Styling

--- src/components/common/CourtOrderPoa.vue
+++ src/components/common/CourtOrderPoa.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="court-order-poa">
+  <section class="pb-6" id="court-order-poa">
     <header>
       <h2>{{sectionNumber}} Court Order and Plan of Arrangement</h2>
       <div class="py-4">
@@ -9,16 +9,18 @@
       </div>
     </header>
     <div :class="{ 'invalid-section': invalidCourtOrder }">
-      <CourtOrderPoaShared
-        id="court-order"
-        :autoValidation="getAppValidate"
-        :draftCourtOrderNumber="getFileNumber"
-        :hasDraftPlanOfArrangement="getHasPlanOfArrangement"
-        :invalidSection="invalidCourtOrder"
-        @emitCourtNumber="setFileNumber($event)"
-        @emitPoa="setHasPlanOfArrangement($event)"
-        @emitValid="setValidCourtOrder($event)"
-      />
+      <v-card flat class="section-container py-6">
+        <CourtOrderPoaShared
+          id="court-order"
+          :autoValidation="getAppValidate"
+          :draftCourtOrderNumber="getFileNumber"
+          :hasDraftPlanOfArrangement="getHasPlanOfArrangement"
+          :invalidSection="invalidCourtOrder"
+          @emitCourtNumber="setFileNumber($event)"
+          @emitPoa="setHasPlanOfArrangement($event)"
+          @emitValid="setValidCourtOrder($event)"
+        />
+      </v-card>
     </div>
   </section>
 </template>
@@ -57,13 +59,4 @@ export default class CourtOrderPoa extends Vue {
 </script>
 
 <style lang="scss" scoped>
-// fix hard-coded whitespace inside shared component
-// we want the same padding as "section-container py-6"
-#court-order {
-  margin-top: 0 !important;
-  padding-top: 0.5rem !important;
-  padding-right: 1.875rem !important;
-  padding-bottom: 1.5rem !important;
-  padding-left: 0.375rem !important;
-}
 </style>
