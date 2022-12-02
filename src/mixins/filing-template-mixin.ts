f65f94462f1edf6ffd30f1702b0b9f417c0f8f2d
Severin Beauvais <severin.beauvais@gmail.com>
2022-12-02 15:38:38 -0800

    - set name translations in snapshot for BC/CCC/ULC

--- src/mixins/filing-template-mixin.ts
+++ src/mixins/filing-template-mixin.ts
@@ -982,7 +982,10 @@ export default class FilingTemplateMixin extends DateMixin {
 
     // handle entity-specific values
     switch (entitySnapshot.businessInfo.legalType) {
-      case CorpTypeCd.BENEFIT_COMPANY: {
+      case CorpTypeCd.BENEFIT_COMPANY:
+      case CorpTypeCd.BC_COMPANY:
+      case CorpTypeCd.BC_CCC:
+      case CorpTypeCd.BC_ULC_COMPANY: {
         // store Name Translations
         if (entitySnapshot.nameTranslations) {
           // don't need cloneDeep because mapNameTranslations already returns new array
