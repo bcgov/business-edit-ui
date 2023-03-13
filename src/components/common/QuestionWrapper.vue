37ccefe439744b9e09f078a8e7a2a2a9f92f0d0f
Jonathan Longe <jlonge@corebox.net>
2023-03-13 14:59:24 -0700

    add relationship question

---
+++ src/components/common/QuestionWrapper.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <header>
+      <h2 :id="id">{{ title }}</h2>
+      <p v-if="subtitle">{{ subtitle }}</p>
+      <p v-if="comment" class="black--text mb-0">{{ comment }}</p>
+    </header>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'QuestionWrapper',
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    comment: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+#question_container {
+  /* Set "header-counter" to 0 */
+  counter-reset: header-counter;
+}
+
+h2::before {
+  /* Increment "header-counter" by 1 */
+  counter-increment: header-counter;
+  content: counter(header-counter) '. ';
+}
+</style>
