3550b933bd8e848ae2a31e1c0db2a538a62bd873
Jonathan Longe <jlonge@corebox.net>
2023-03-14 17:44:27 -0700

    add more restoration endpoints

--- src/enums/routeNames.ts
+++ src/enums/routeNames.ts
@@ -4,7 +4,10 @@ export enum RouteNames {
   CHANGE = 'change',
   CONVERSION = 'conversion',
   CORRECTION = 'correction',
-  RESTORATION = 'restoration',
+  RESTORATION_FULL = 'fullRestoration',
+  RESTORATION_LIMITED = 'limitedRestoration',
+  RESTORATION_EXTENSION = 'limitedRestorationExtension',
+  RESTORATION_CONVERSION = 'limitedRestorationToFull',
   SIGN_IN = 'signin',
   SIGN_OUT = 'signout',
   SPECIAL_RESOLUTION = 'special-resolution'
