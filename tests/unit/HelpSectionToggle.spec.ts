e4812963312f9870d7f1a5d0f5bd427ce851b49a
Jia Xu <Jia.Xu@gov.bc.ca>
2023-07-21 14:25:27 -0700

    build error fix, a unit test updated

--- tests/unit/HelpSectionToggle.spec.ts
+++ tests/unit/HelpSectionToggle.spec.ts
@@ -1,14 +1,27 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+import { mount, shallowMount } from '@vue/test-utils'
+import { createPinia, setActivePinia } from 'pinia'
 import HelpSectionToggle from '@/components/Alteration/HelpSectionToggle.vue'
 
-const localVue = createLocalVue()
+const vuetify = new Vuetify({})
+
+setActivePinia(createPinia())
 
 describe('HelpSectionToggle component', () => {
+  it('renders itself', () => {
+    const wrapper = mount(HelpSectionToggle, { vuetify })
+
+    expect(wrapper.findComponent(HelpSectionToggle).exists()).toBe(true)
+
+    wrapper.destroy()
+  })
+
   it('toggles the help section when clicked', async () => {
-    const wrapper = shallowMount(HelpSectionToggle, { localVue })
-    expect(wrapper.vm.isHelpVisible).toBe(false)
-    await wrapper.setData({ isHelpVisible: true })
-    await localVue.nextTick()
-    expect(wrapper.vm.isHelpVisible).toBe(true)
+    const wrapper = shallowMount(HelpSectionToggle)
+    const vm = wrapper.vm as any
+    expect(vm.isHelpVisible).toBe(false)
+    await wrapper.find('.help-toggle').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(vm.isHelpVisible).toBe(true)
   })
 })
