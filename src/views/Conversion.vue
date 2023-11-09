8ca35c56a82ddc2b8e74f599322721385efee219
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2023-11-09 13:23:37 -0800

    - app version = 4.7.6 (#542)
    - deleted obsolete "alteration-ui-enabled" FF
    - deleted obsolete "change-ui-enabled" FF
    - deleted obsolete "conversion-ui-enabled" FF
    - removed flag usage from Alteration.vue
    - removed flag usage from Change.vue
    - removed flag usage from Conversion.vue
    - removed flag usage from SpecialResolution.vue

--- src/views/Conversion.vue
+++ src/views/Conversion.vue
@@ -64,7 +64,6 @@
 <script lang="ts">
 import { Component, Emit, Mixins, Prop, Watch } from 'vue-property-decorator'
 import { Action, Getter } from 'pinia-class'
-import { GetFeatureFlag } from '@/utils/'
 import { ConversionNOB, ConversionSummary } from '@/components/Conversion'
 import { CompletingParty, BusinessStartDate, BusinessType, EntityName, FolioInformation, OfficeAddresses,
   PeopleAndRoles, YourCompanyWrapper } from '@/components/common/'
@@ -137,14 +136,6 @@ export default class Conversion extends Mixins(CommonMixin, FeeMixin, FilingTemp
 
     // do not proceed if we are not authenticated (safety check - should never happen)
     if (!this.isAuthenticated) return
-
-    // do not proceed if FF is disabled
-    // bypass this when Vitest is running as FF are not fetched
-    if (!this.isVitestRunning && !GetFeatureFlag('conversion-ui-enabled')) {
-      window.alert('Conversion filings are not available at the moment. Please check again later.')
-      this.$root.$emit('go-to-dashboard', true)
-      return
-    }
 
     // do not proceed if user is not staff
     const isStaffOnly = this.$route.matched.some(r => r.meta?.isStaffOnly)
