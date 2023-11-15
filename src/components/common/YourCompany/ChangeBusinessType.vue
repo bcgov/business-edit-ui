00a2612fb2375ef753749d35cdd4fcaac03545d7
Travis Semple <travis8814@gmail.com>
2023-11-15 14:37:34 -0800

    Name request check error update

--- src/components/common/YourCompany/ChangeBusinessType.vue
+++ src/components/common/YourCompany/ChangeBusinessType.vue
@@ -388,7 +388,8 @@ export default class ChangeBusinessType extends Mixins(CommonMixin) {
       return true
     }
     // Named ULC to BC Limited require a name request.
-    if (this.getEntitySnapshot?.businessInfo?.legalType === CorpTypeCd.BC_ULC_COMPANY && this.isBcLimited) {
+    if (this.getEntitySnapshot?.businessInfo?.legalType === CorpTypeCd.BC_ULC_COMPANY &&
+        (this.isBcLimited || this.isBenefitCompany)) {
       return true
     }
     return false
